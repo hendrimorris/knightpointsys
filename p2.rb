c6d7de8e226f0a46987e22c942e1e583473fe817
lucheski <lucheski@achieve3000s-MacBook-Pro-181.local>
2017-12-28 11:32:56 -0500

    added comments

--- p2.rb
+++ p2.rb
@@ -1,12 +1,14 @@
+#!/usr/bin/env ruby
+
+# Loops through all instances in specified region and prints instance attributes
 require 'aws-sdk-ec2'  # v2: require 'aws-sdk'
 
 ec2 = Aws::EC2::Resource.new(region: 'us-east-1')
 
-# To only get the first 10 instances:
-# ec2.instances.limit(10).each do |i|
 ec2.instances.each do |i|
   puts "ID:    #{i.id}"
   puts "Tags:    #{i.tags}"
   puts "State: #{i.state.name}"
   puts "LocalIP: #{i.private_ip_address}"
 end
+
